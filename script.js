e9434f25a6b0455acb12eef14004733838f5c24a
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-21 12:41:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var onresize = function() {
+   //your code here
+   //this is just an example
+   width = document.body.clientWidth;
+   height = document.body.clientHeight;
+}
+window.addEventListener("resize", onresize);
