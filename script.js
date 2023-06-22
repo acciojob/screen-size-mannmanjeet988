ce1fe8b901eb6715a1631a170d1a7c088769ee1e
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-22 17:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var onresize = function() {
    //your code here
    //this is just an example
    width = window.innerWidth;
-   height =  window.inneHeight;
+   height =  window.innerHeight;
 	divElement= document.querySelector("h1");
 	divElement.innerHTML=`Width: ${width} and Height: ${height}`;
 }
